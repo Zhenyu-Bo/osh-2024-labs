2735b3b37270e4b1a4c62ec83a17cbd4fbb2fc3e
Zhenyu-Bo <bzy1117@mail.ustc.edu.cn>
2024-03-08 16:15:45 +0800

    update for make debug

--- lab0/bubblesort.cpp
+++ lab0/bubblesort.cpp
@@ -7,7 +7,7 @@ void bubbleSort(std::vector<int>& arr) {
         for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
                 std::swap(arr[j], arr[j + 1]);
-                #ifdef PRINT
+                #ifdef PRINT // 根据是否定义了PRINT宏来判断是否打印
                 for(auto x: arr) {
                     std::cout << x <<  " ";
                 }
