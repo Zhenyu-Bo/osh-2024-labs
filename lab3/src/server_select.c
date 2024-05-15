7afe70a5f51f1288064f264a9db5b58db53190c4
Zhenyu-Bo <bzy1117@mail.ustc.edu.cn>
2024-05-15 18:22:28 +0800

    fix bug about method is POST

--- lab3/src/server_select.c
+++ lab3/src/server_select.c
@@ -13,6 +13,7 @@
 #include <ctype.h>
 #include <sys/stat.h>
 #include <sys/select.h>
+#include <errno.h>
 
 #define BIND_IP_ADDR "127.0.0.1"
 #define BIND_PORT 8000
@@ -20,14 +21,19 @@
 #define MAX_SEND_LEN 1048576
 #define MAX_PATH_LEN 1024
 #define MAX_HOST_LEN 1024
-#define MAX_CONN 20
-#define THREAD_POOL_SIZE 20
+#define MAX_CONN 100
+#define BUFFER_SIZE 4096
 
 #define HTTP_STATUS_200 "200 OK"
 #define HTTP_STATUS_404 "404 Not Found"
 #define HTTP_STATUS_500 "500 Internal Server Error"
 
 int serv_sock;
+
+void print_perror(char *str) {
+    perror(str);
+    exit(1);
+}
 
 void divide_request(char *req, ssize_t req_len, char *method,char *url,char *version,char *host)
 {
@@ -70,11 +76,12 @@ int parse_request(char* request, ssize_t req_len, char* path, ssize_t* path_len,
 
     divide_request(req, req_len, method, url, version, host);
     ssize_t url_len = strlen(url);
+    ssize_t host_len = strlen(host);
     //ssize_t ver_len = strlen(version);
     // 如果请求的方法不为GET/请求的URL不以'/'开头/请求的版本号不为HTTP/开头或不以\r\n结尾，则返回错误
     if (strcmp(method,"GET") != 0 || url[0] != '/' ||
         (strncmp(version,"HTTP/1.0\r\n",10) != 0 && strncmp(version, "HTTP/1.1\r\n",10) != 0) ||
-        strncmp(host, "Host: 127.0.0.1:8000\r\n", 22) != 0) {
+        strncmp(host, "Host: ", 6) != 0 || host[host_len-2] != '\r' || host[host_len-1] != '\n') {
         return -1;
     }
     memcpy(path,url,url_len+1);
@@ -82,8 +89,7 @@ int parse_request(char* request, ssize_t req_len, char* path, ssize_t* path_len,
     return 0;
 }
 
-
-int get_content(char *path, long *file_size, char **content)
+int get_content(char *path, long *file_size, FILE **file)
 {
     char cur_dir[MAX_PATH_LEN];
     if(getcwd(cur_dir,sizeof(cur_dir)) == NULL) {
@@ -101,8 +107,6 @@ int get_content(char *path, long *file_size, char **content)
         return -1;
     }
     snprintf(file_path, 2*MAX_PATH_LEN, "%s%s", cur_dir, path);
-
-    *content = NULL;
 
     struct stat path_stat;
     if(stat(file_path, &path_stat) == -1) {
@@ -133,37 +137,23 @@ int get_content(char *path, long *file_size, char **content)
         return -1;
     }
     
-    FILE *file = fopen(file_path, "r");
-    if (file == NULL) {
+    *file = fopen(file_path, "r");
+    if (*file == NULL) {
         perror("fopen error!\n");
         free(file_path);
         return -2;
     }
 
-    fseek(file, 0, SEEK_END);
-    *file_size = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    *content = (char*) malloc((*file_size + 1) * sizeof(char));
-    if(*content == NULL) {
-        perror("malloc error!\n");
-        fclose(file);
-        return -1;
-    }
-
-    fread(*content, 1, *file_size, file);
-    (*content)[*file_size] = '\0';
-
-    fclose(file);
+    fseek(*file, 0, SEEK_END);
+    *file_size = ftell(*file);
+    fseek(*file, 0, SEEK_SET);
 
     free(file_path);
-
     return 0;
 }
 
-void write_response(char *response, int ret, int ret_2, long file_size, char *content) 
+int write_response(char *response, int ret, int ret_2, long file_size, int clnt_sock) 
 {
-    size_t status_200_len = strlen("HTTP/1.0 200 OK\r\nContent-Length: \r\n\r\n");
     if(ret == -1 || ret_2 == -1) {
         sprintf(response,
             "HTTP/1.0 %s \r\nContent-Length: 0\r\n\r\n",
@@ -175,19 +165,23 @@ void write_response(char *response, int ret, int ret_2, long file_size, char *co
             HTTP_STATUS_404);
     }
     else if(ret == 0 && ret_2 == 0) {
-        size_t total_len = status_200_len + sizeof(file_size) + strlen(content) + 1;
-        if(total_len > MAX_SEND_LEN) {
-            char *new_response = (char *)realloc(response, total_len * sizeof(char));
-            if(!new_response) {
-                perror("realloc error!\n");
-                return;
-            }
-            response = new_response;
-        }
         sprintf(response,
-            "HTTP/1.0 %s\r\nContent-Length: %zd\r\n\r\n%s",
-            HTTP_STATUS_200, file_size, content);
+            "HTTP/1.0 %s\r\nContent-Length: %zd\r\n\r\n",
+            HTTP_STATUS_200, file_size);
     }
+    ssize_t response_len = strlen(response);
+
+    // 写入响应，直到所有数据都被写入
+    ssize_t write_len = 0;
+    while(response_len > 0) {
+        if((write_len = write(clnt_sock, response, response_len)) < 0) {
+            perror("write error!\n");
+            return -1;
+        }
+        response += write_len;
+        response_len -= write_len;
+    }
+    return 0;
 }
 
 void handle_clnt(int clnt_sock)
@@ -195,6 +189,11 @@ void handle_clnt(int clnt_sock)
     // 读取客户端发送来的数据，并解析
     char* req_buf = (char*) malloc(MAX_RECV_LEN * sizeof(char));
     char* version = (char*) malloc(MAX_RECV_LEN * sizeof(char));
+    // 构造要返回的数据
+    // 注意，响应头部后需要有一个多余换行（\r\n\r\n），然后才是响应内容
+    char* response = (char*) malloc(MAX_SEND_LEN * sizeof(char));
+    // 根据 HTTP 请求的内容，解析资源路径和 Host 头
+    char* path = (char*) malloc(MAX_PATH_LEN * sizeof(char));
 
     ssize_t req_len = 0;
     ssize_t n = 0;
@@ -203,11 +202,15 @@ void handle_clnt(int clnt_sock)
     while (1) {
         n = read(clnt_sock, req_buf + req_len, max_recv_len - req_len);
         if(n < 0) {
+            if(errno == EINTR) continue;
             perror("read error!\n");
-            free(req_buf);
-            return;
+            goto end;
         }
         req_len += n;
+        if(strlen(req_buf) >= 3 && strncmp(req_buf, "GET", 3) != 0) {
+            write_response(response, -1, -1, 0, clnt_sock);
+            goto end;
+        }
         if (req_len >= 4 && strcmp(req_buf + req_len - 4, "\r\n\r\n") == 0) {
             break;
         }
@@ -216,73 +219,67 @@ void handle_clnt(int clnt_sock)
             char *new_req_buf = (char *)realloc(req_buf, max_recv_len * sizeof(char));
             if(!new_req_buf) {
                 perror("realloc error!\n");
-                free(req_buf);
-                return;
+                goto end;
             }
             req_buf = new_req_buf;
         }   
     }
 
-    // 根据 HTTP 请求的内容，解析资源路径和 Host 头
-    char* path = (char*) malloc(MAX_PATH_LEN * sizeof(char));
+    
     ssize_t path_len;
-    int ret = parse_request(req_buf, req_len, path, &path_len, version);
+    int ret_1 = parse_request(req_buf, req_len, path, &path_len, version);
     long file_size;
-    char* content = NULL;
-    int ret_2 = get_content(path, &file_size, &content);
-    // 构造要返回的数据
-    // 注意，响应头部后需要有一个多余换行（\r\n\r\n），然后才是响应内容
-    char* response = (char*) malloc(MAX_SEND_LEN * sizeof(char)) ;
-    size_t response_len;
-    write_response(response, ret, ret_2, file_size, content);
-    response_len = strlen(response);
-
-    // 写入响应，直到所有数据都被写入
-    ssize_t total_written = 0;
-    while (total_written < response_len) {
-        n = write(clnt_sock, response + total_written, response_len - total_written);
-        if(n < 0) {
-            perror("write error!\n");
-            goto end;
-        }
-        total_written += n;
+    //char* content = NULL;
+    FILE *file = NULL;
+    int ret_2 = get_content(path, &file_size, &file);
+    
+    if(write_response(response, ret_1, ret_2, file_size, clnt_sock) == -1){
+        goto end;
     }
-
+    if(ret_2 == 0) {
+        char buffer[BUFFER_SIZE];
+        while (!feof(file)) {
+            size_t n = fread(buffer, 1, BUFFER_SIZE, file);
+            if (n < 0) {
+                perror("read error!\n");
+                break;
+            }
+            ssize_t write_len = 0;
+            while (write_len < n) {
+                ssize_t ret = write(clnt_sock, buffer + write_len, n - write_len);
+                if (ret < 0) {
+                    if(errno == EINTR) continue;
+                    perror("write error!\n");
+                    break;
+                }
+                write_len += ret;
+            }
+        }
+    }
+    //fclose(file);
     // 关闭客户端套接字
-    close(clnt_sock);
-
-    // 释放内存
 end:
+    close(clnt_sock);
+    // 释放内存
     free(req_buf);
     free(path);
     free(response);
     free(version);
-    free(content);
+    if(file != NULL)
+        free(file);
     return;
-}
-
-void handle_signal(int sig)
-{
-    if(sig == SIGINT)
-    {
-        // 关闭套接字
-        close(serv_sock);
-        // 打印换行符，使得输出更加美观
-        printf("\n");
-        //结束程序
-        exit(0);
-    }
 }
 
 int main(){
     // 注册信号处理函数
-    signal(SIGINT, handle_signal);
+    //signal(SIGINT, handle_signal);
     // 创建套接字，参数说明：
     //   AF_INET: 使用 IPv4
     //   SOCK_STREAM: 面向连接的数据传输方式
     //   IPPROTO_TCP: 使用 TCP 协议
     //int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if((serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1)
+        print_perror("socket error!\n");
 
     // 将套接字和指定的 IP、端口绑定
     //   用 0 填充 serv_addr（它是一个 sockaddr_in 结构体）
@@ -295,11 +292,13 @@ int main(){
     serv_addr.sin_addr.s_addr = inet_addr(BIND_IP_ADDR);
     serv_addr.sin_port = htons(BIND_PORT);
     //   绑定
-    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        print_perror("bind error!\n");
 
     // 使得 serv_sock 套接字进入监听状态，开始等待客户端发起请求
-    listen(serv_sock, MAX_CONN);
-
+    if(listen(serv_sock, MAX_CONN) == -1)
+        print_perror("listen error!\n");
+    
     fd_set reads, temps;
     FD_ZERO(&reads);
     FD_SET(serv_sock, &reads);
